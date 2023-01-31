ef0d22a3bd73f7cad06b1e84306f17cd73a73f62
Isaac Hussain <isaaclearns58@gmail.com>
2023-01-31 12:20:30 +0300

    Adds UI design for PersonalInformation

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class Input extends Component {
   constructor(props) {
     super(props);
   }
 
+  static get propTypes() {
+    return {
+      children: PropTypes.any,
+      inputName: PropTypes.string,
+    };
+  }
+
   render() {
     const { inputName } = this.props;
-    return <input className='' placeholder={inputName} />;
+    return <input className='px-3 py-1 mb-2 rounded' placeholder={inputName} />;
   }
 }
 
--- src/components/PersonalInformation.js
+++ src/components/PersonalInformation.js
@@ -1,28 +1,28 @@
 import React, { Component } from "react";
-// import Input from "./Input";
+import Input from "./Input";
 
 class PersonalInformation extends Component {
   render() {
     return (
-      <>
-        <h1 className='text-xl font-medium'>Personal Information</h1>
-        {/* <Input inputName='First name' /> */}
+      <div className='flex flex-col '>
+        <h1 className='text-xl font-medium mb-4'>Personal Information</h1>
+        <Input inputName='First name' />
+        <Input inputName='Last name' />
+        <Input inputName='Title' />
+        <Input inputName='Photo' />
+        <Input inputName='Address' />
+        <Input inputName='Phone number' />
+        <Input inputName='Email' />
 
-        {/* <input type='text' placeholder='First name' />
-        <input type='text' placeholder='Last name' />
-        <input type='text' placeholder='Title' />
-        <input type='text' placeholder='Photo' />
-        <input type='text' placeholder='Address' />
-        <input type='text' placeholder='Phone number' />
-        <input type='text' placeholder='Email' /> */}
         <textarea
+          className='px-3 py-1 mb-2 rounded resize-y'
           name='description'
           id='description'
           cols='30'
-          rows='10'
-          placeholder='description'
+          rows='3'
+          placeholder='Description'
         ></textarea>
-      </>
+      </div>
     );
   }
 }
