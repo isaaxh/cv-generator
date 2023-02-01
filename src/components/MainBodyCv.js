84f3cf34ea1cc95798538d0ed688e10dc949484d
Isaac Hussain <isaaclearns58@gmail.com>
2023-02-01 08:46:52 +0300

    Adds styling to the descipton cv

--- src/components/MainBodyCv.js
+++ src/components/MainBodyCv.js
@@ -3,7 +3,31 @@ import React, { Component } from "react";
 class MainBodyCv extends Component {
   render() {
     return (
-      <div className='bg-yellow-300 col-span-2 row-span-5'>MainBodyCv</div>
+      <div
+        className='col-span-2 row-span-5 p-5 [&>div]:mb-5 [&>div>hr]:border-solid 
+        [&>div>hr]:border-1 [&>div>hr]:border-gray-300 [&>div>h2]:text-blue-900 
+      [&>div>h2]:text-md [&>div>h2]:font-semibold [&>div>hr]:pb-2'
+      >
+        <div className=''>
+          <h2>Description</h2>
+          <hr />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+            aliquid aspernatur dolore laudantium cumque sunt ut ullam ad
+            pariatur iste. Ut accusamus amet hic soluta incidunt velit sequi
+            optio dolor! Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Iure id esse numquam similique nam quia cupiditate officiis
+          </p>
+        </div>
+        <div>
+          <h2>Experience</h2>
+          <hr />
+        </div>
+        <div>
+          <h2>Education</h2>
+          <hr />
+        </div>
+      </div>
     );
   }
 }
