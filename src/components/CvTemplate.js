6d226fd73e820ace088ce4e78e7c910818b75191
Isaac Hussain <isaaclearns58@gmail.com>
2023-02-01 05:41:52 +0300

    Start designing the smaller components of cv template

--- src/components/CvTemplate.js
+++ src/components/CvTemplate.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export default class CvTemplate extends Component {
   render() {
     return (
-      <section className='bg-zinc-100 h-[1122px] w-[794] max-w-4xl flex-1 shadow-2xl rounded p-5 lg:p-9'>
+      <section className='bg-zinc-100 h-[1122px] w-[794] max-w-4xl flex-1 shadow-2xl rounded p-5 lg:p-9 lg:sticky lg:top-0'>
         CvTemplate
       </section>
     );
