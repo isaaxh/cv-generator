c20439c5465fed75b99efe28c5caffa6f85a8594
Isaac Hussain <isaaclearns58@gmail.com>
2023-02-01 06:49:09 +0300

    Adds styling for HeaderCv

--- src/components/HeaderCv.js
+++ src/components/HeaderCv.js
@@ -2,7 +2,15 @@ import React, { Component } from "react";
 
 class HeaderCv extends Component {
   render() {
-    return <div className='bg-gray-300 col-span-3 '>HeaderCv</div>;
+    return (
+      <div className='bg-blue-900 [&>*]:text-white col-span-3 flex flex-col justify-center p-6'>
+        <div className='flex [&>*]:text-6xl [&>*]:font-bold'>
+          <h1>FIRST</h1>
+          <h1>LAST</h1>
+        </div>
+        <h2 className='text-2xl'>Software Engineer</h2>
+      </div>
+    );
   }
 }
 
