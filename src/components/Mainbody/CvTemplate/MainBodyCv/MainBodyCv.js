ed3534136e63a2deb0c1a9ca653b1fd0d177aefd
Isaac Hussain <isaaclearns58@gmail.com>
2023-02-16 13:59:36 +0300

    refactor: remove sample instance

--- src/components/Mainbody/CvTemplate/MainBodyCv/MainBodyCv.js
+++ src/components/Mainbody/CvTemplate/MainBodyCv/MainBodyCv.js
@@ -26,61 +26,44 @@ export default function MainBodyCv() {
       <div>
         <h2>Experience</h2>
         <hr />
-        {expInstances.length > 0 ? (
-          expInstances.map((instance) => {
-            return (
-              <ExpInstance
-                key={instance.key}
-                stateObject={instance}
-                position={instance.position}
-                companyName={instance.company}
-                cityName={instance.cityExp}
-                from={instance.fromExp}
-                to={instance.toExp}
-                handleRetrieve={handleRetrieveInstance}
-              />
-            );
-          })
-        ) : (
-          <ExpInstance
-            position={"Software Engineer"}
-            companyName={"ABC Inc."}
-            cityName={"Kuala Lumpur"}
-            from={"2010"}
-            to={"2014"}
-          />
-        )}
+        {expInstances.length > 0
+          ? expInstances.map((instance) => {
+              return (
+                <ExpInstance
+                  key={instance.key}
+                  stateObject={instance}
+                  position={instance.position}
+                  companyName={instance.company}
+                  cityName={instance.cityExp}
+                  from={instance.fromExp}
+                  to={instance.toExp}
+                  handleRetrieve={handleRetrieveInstance}
+                />
+              );
+            })
+          : null}
       </div>
       <div>
         <h2>Education</h2>
         <hr />
-        {eduInstances.length > 0 ? (
-          eduInstances.map((instance) => {
-            return (
-              <EduInstance
-                key={instance.key}
-                stateObject={instance}
-                id={instance.key}
-                uniName={instance.uniName}
-                cityName={instance.cityEdu}
-                degreeName={instance.degree}
-                subjectName={instance.subject}
-                from={instance.fromEdu}
-                to={instance.toEdu}
-                handleRetrieve={handleRetrieveInstance}
-              />
-            );
-          })
-        ) : (
-          <EduInstance
-            uniName={"University of Boston"}
-            cityName={"Boston"}
-            degreeName={"Computer Science"}
-            subjectName={"Programming"}
-            from={"2020"}
-            to={"2024"}
-          />
-        )}
+        {eduInstances.length > 0
+          ? eduInstances.map((instance) => {
+              return (
+                <EduInstance
+                  key={instance.key}
+                  stateObject={instance}
+                  id={instance.key}
+                  uniName={instance.uniName}
+                  cityName={instance.cityEdu}
+                  degreeName={instance.degree}
+                  subjectName={instance.subject}
+                  from={instance.fromEdu}
+                  to={instance.toEdu}
+                  handleRetrieve={handleRetrieveInstance}
+                />
+              );
+            })
+          : null}
       </div>
     </div>
   );
